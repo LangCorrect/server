2b216907ae67a21f42259be470100b13bb7320d6
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-07-19 03:27:17 -0700

    init

---
+++ langcorrect/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from langcorrect.users.models import User
+from langcorrect.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
---
+++ langcorrect/users/api/serializers.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+from langcorrect.users.models import User as UserType
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer[UserType]):
+    class Meta:
+        model = User
+        fields = ["username", "name", "url"]
+
+        extra_kwargs = {
+            "url": {"view_name": "api:user-detail", "lookup_field": "username"},
+        }
