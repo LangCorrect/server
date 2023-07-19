58d8437c36fd50ebe652234638ef51e4ea5fcfc6
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-07-19 05:55:46 -0700

    Fix broken tests

--- langcorrect/users/tests/factories.py
+++ langcorrect/users/tests/factories.py
@@ -9,7 +9,8 @@ from factory.django import DjangoModelFactory
 class UserFactory(DjangoModelFactory):
     username = Faker("user_name")
     email = Faker("email")
-    name = Faker("name")
+    first_name = Faker("first_name")
+    last_name = Faker("last_name")
 
     @post_generation
     def password(self, create: bool, extracted: Sequence[Any], **kwargs):
