0c5aa81608d41e202800222be9a4b7ade9347400
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-07-20 19:29:16 -0700

    Migrate data from journal.Post to posts.Post

--- langcorrect/posts/models.py
+++ langcorrect/posts/models.py
@@ -1,3 +1,52 @@
-from django.db import models  # noqa: 402
+from django.conf import settings
+from django.db import models
+from django.utils.text import slugify
+from django.utils.translation import gettext_lazy as _
+from model_utils.models import SoftDeletableModel, TimeStampedModel
+from taggit.managers import TaggableManager
 
-# Create your models here.
+from langcorrect.users.models import GenderChoices
+
+
+class PostVisibility(models.TextChoices):
+    PUBLIC = "public", _("Viewable by everyone")
+    MEMBER = "member", _("Viewable only by registered members")
+
+
+class Post(TimeStampedModel, SoftDeletableModel):
+    class Meta:
+        ordering = ["-created"]
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=60)
+    text = models.TextField()
+    native_text = models.TextField()
+    language = models.ForeignKey(
+        "languages.Language",
+        on_delete=models.CASCADE,
+    )
+    gender_of_narration = models.CharField(choices=GenderChoices.choices, default=GenderChoices.UNKNOWN, max_length=1)
+    permission = models.CharField(choices=PostVisibility.choices, default=PostVisibility.PUBLIC, max_length=6)
+    is_draft = models.BooleanField(default=False)
+    prompt = models.ForeignKey("prompts.Prompt", on_delete=models.SET_NULL, null=True, blank=True)
+    slug = models.SlugField(max_length=255, null=True)
+    tags = TaggableManager(blank=True)
+    language_level = models.CharField(max_length=30, null=True, blank=True)
+    is_corrected = models.IntegerField(default=0)  # 0-False 1-True
+
+    def save(self, *args, **kwargs):
+        if not self.slug and not self.is_draft:
+            new_slug = generated_slug = slugify(self.title, allow_unicode=True)
+
+            if not new_slug:
+                count = 1
+                generated_slug = f"-{count}"
+
+            if Post.all_objects.filter(slug=new_slug).exists() or not new_slug:
+                count = 1
+                while Post.all_objects.exclude(id=self.pk).filter(slug=generated_slug).exists():
+                    generated_slug = f"{new_slug}-{count}"
+                    count += 1
+            self.slug = generated_slug
+
+        super().save(*args, **kwargs)
