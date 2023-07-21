83b1541caee66b469ecc1058edcf79619e5672fc
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-07-21 00:58:01 -0700

    Register admin for corrections

--- langcorrect/corrections/admin.py
+++ langcorrect/corrections/admin.py
@@ -1,3 +1,30 @@
-from django.contrib import admin  # noqa: 401
+from django.contrib import admin
 
-# Register your models here.
+from langcorrect.corrections.models import CorrectedRow, CorrectionType
+
+
+@admin.register(CorrectionType)
+class CorrectionTypeAdmin(admin.ModelAdmin):
+    list_display = ["name", "description"]
+
+
+@admin.register(CorrectedRow)
+class CorrectedRowAdmin(admin.ModelAdmin):
+    readonly_fields = ["post", "user", "post_row", "correction", "note", "correction_types"]
+    list_display = ["author", "corrector", "original_sentence", "correction", "note", "get_correction_types"]
+    search_fields = ["post__user__username"]
+
+    def author(self, obj):
+        if obj.post:
+            return obj.post.user
+        else:
+            return None
+
+    def corrector(self, obj):
+        return obj.user
+
+    def original_sentence(self, obj):
+        return obj.post_row.sentence
+
+    def get_correction_types(self, obj):
+        return [t.name for t in obj.correction_types.all()]
