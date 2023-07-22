7db6687897e320199a644c2a9a12137bfd7c06c5
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-07-21 20:54:48 -0700

    Start new Django app 'follows'

--- config/settings/base.py
+++ config/settings/base.py
@@ -96,6 +96,7 @@ LOCAL_APPS = [
     "langcorrect.prompts",
     "langcorrect.posts",
     "langcorrect.corrections",
+    "langcorrect.follows",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
---
+++ langcorrect/follows/models.py
@@ -0,0 +1,3 @@
+from django.db import models  # noqa: 401
+
+# Create your models here.
