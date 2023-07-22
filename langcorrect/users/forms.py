8af603a8f5fa2c92883146d811577e7b167d2525
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-07-21 23:38:09 -0700

    Add additional fields to signup form

--- langcorrect/users/forms.py
+++ langcorrect/users/forms.py
@@ -1,8 +1,12 @@
 from allauth.account.forms import SignupForm
 from allauth.socialaccount.forms import SignupForm as SocialSignupForm
+from django import forms as django_forms
 from django.contrib.auth import forms as admin_forms
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
+
+from langcorrect.languages.models import Language, LanguageLevel, LevelChoices
+from langcorrect.users.models import GenderChoices
 
 User = get_user_model()
 
@@ -31,6 +35,48 @@ class UserSignupForm(SignupForm):
     Default fields will be added automatically.
     Check UserSocialSignupForm for accounts created from social.
     """
+
+    studying_level_options = list(LevelChoices.choices)[:-1]
+    native_language = django_forms.ModelChoiceField(queryset=Language.objects.all())
+    studying_language = django_forms.ModelChoiceField(queryset=Language.objects.all())
+    studying_language_level = django_forms.ChoiceField(choices=studying_level_options)
+    gender_of_narration = django_forms.ChoiceField(choices=GenderChoices.choices)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.initial["gender_of_narration"] = GenderChoices.UNKNOWN
+
+    def clean(self):
+        cleaned_data = super().clean()
+        native_language = cleaned_data.get("native_language")
+        studying_language = cleaned_data.get("studying_language")
+
+        if native_language == studying_language:
+            self.add_error(
+                "studying_language",
+                django_forms.ValidationError(
+                    "Studying and native language cannot be the same",
+                    code="dupe_languages",
+                ),
+            )
+        return cleaned_data
+
+    def save(self, request):
+        user = super().save(request)
+        native_language = self.cleaned_data.get("native_language")
+        studying_language = self.cleaned_data.get("studying_language")
+        studying_language_level = self.cleaned_data.get("studying_language_level")
+
+        # native
+        LanguageLevel.objects.create(user=user, language=native_language, level=LevelChoices.NATIVE)
+
+        # studying
+        LanguageLevel.objects.create(user=user, language=studying_language, level=studying_language_level)
+
+        user.gender = self.cleaned_data.get("gender_of_narration")
+        user.save()
+
+        return user
 
 
 class UserSocialSignupForm(SocialSignupForm):
