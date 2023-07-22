b38ffe18ddcf39f8a23553a584f552cf8446a760
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-07-21 21:39:07 -0700

    Register admin for Membership

--- langcorrect/memberships/admin.py
+++ langcorrect/memberships/admin.py
@@ -1,3 +1,22 @@
-from django.contrib import admin  # noqa: 401
+from django.contrib import admin
 
-# Register your models here.
+from langcorrect.memberships.models import Membership
+
+
+@admin.register(Membership)
+class MembershipAdmin(admin.ModelAdmin):
+    list_display = [
+        "user",
+        "customer_code",
+        "subscription_code",
+        "plan_type",
+        "billing_cycle_ends",
+        "cancelled_date",
+    ]
+    list_filter = ["plan_type"]
+    search_fields = [
+        "plan_type",
+        "user__username",
+        "customer_code",
+        "subscription_code",
+    ]
