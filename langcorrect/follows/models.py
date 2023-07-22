9d44fe3352da829345124cdec136da825e17c35e
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-07-21 21:04:13 -0700

    Migrate data account.Followers to follows.Followers

--- langcorrect/follows/models.py
+++ langcorrect/follows/models.py
@@ -1,3 +1,12 @@
-from django.db import models  # noqa: 401
+from django.conf import settings
+from django.db import models
+from model_utils.models import SoftDeletableModel, TimeStampedModel
 
-# Create your models here.
+
+class Follower(TimeStampedModel, SoftDeletableModel):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="follower")
+    follow_to = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="follow_to")
+    get_notification = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.user} is following {self.follow_to}"
