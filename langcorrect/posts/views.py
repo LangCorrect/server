1c883c7ac2aeef4b392d432cd8c8c156487fddc7
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-07-22 02:09:53 -0700

    [wip] Add layout for post list

--- langcorrect/posts/views.py
+++ langcorrect/posts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render  # noqa: 402
+from django.views.generic import ListView
 
-# Create your views here.
+from langcorrect.posts.models import Post
+
+
+class PostListView(ListView):
+    model = Post
+    slug_field = "slug"
+    slug_url_kwarg = "slug"
+    paginate_by = 25
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            context["following_feed"] = Post.available_objects.filter(user__in=current_user.following_users)[:5]
+
+        return context
+
+
+post_list_view = PostListView.as_view()
