08ba0800f72ca137a25501fe85c62f045e3f4db5
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-07-21 21:20:07 -0700

    migrate data contribution.Contribution to contributions.Contribution

--- langcorrect/contributions/models.py
+++ langcorrect/contributions/models.py
@@ -1,3 +1,13 @@
-from django.db import models  # noqa: 401
+from django.conf import settings
+from django.db import models
+from model_utils.models import SoftDeletableModel, TimeStampedModel
 
-# Create your models here.
+
+class Contribution(SoftDeletableModel, TimeStampedModel):
+    class Meta:
+        ordering = ["-total_points"]
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    total_points = models.IntegerField(default=0)
+    post_count = models.IntegerField(default=0)
+    correction_count = models.IntegerField(default=0)
