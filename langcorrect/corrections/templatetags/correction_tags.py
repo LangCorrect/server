ed608e53a7c258fc21166862afa381081d37c8c9
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-07-25 23:15:07 -0700

    Fix language match not displaying

--- langcorrect/corrections/templatetags/correction_tags.py
+++ langcorrect/corrections/templatetags/correction_tags.py
@@ -4,12 +4,13 @@ register = template.Library()
 
 
 @register.inclusion_tag("corrections/partials/user_correction_card.html")
-def render_user_correction_card(user, post, correction_data):
+def render_user_correction_card(user, current_user, post, correction_data):
     corrections = correction_data.get("corrections")
     replies = correction_data.get("replies")
     overall_feedback = correction_data.get("overall_feedback")
     return {
         "user": user,
+        "current_user": current_user,
         "post": post,
         "corrections": corrections,
         "replies": replies,
