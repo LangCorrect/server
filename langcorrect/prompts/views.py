63722518754d68a976a05a49bfce240bb02972fe
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-08-07 14:24:49 -0700

    Filter prompt responses by language code

--- langcorrect/prompts/views.py
+++ langcorrect/prompts/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView, ListView
 
+from langcorrect.languages.models import Language
 from langcorrect.prompts.models import Prompt
 
 
@@ -62,6 +63,31 @@ class PromptDetailView(LoginRequiredMixin, DetailView):
     slug_field = "slug"
     slug_url_kwarg = "slug"
     paginate_by = 25
+
+    def get_lang_code(self):
+        return self.request.GET.get("lang_code", None)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        prompt_responses = self.get_object().post_set.all()
+
+        response_languages = list(prompt_responses.values_list("language__code", flat=True).distinct().order_by())
+
+        selected_lang_code = self.get_lang_code()
+        language_filter_choices = Language.objects.filter(code__in=response_languages)
+
+        if selected_lang_code and selected_lang_code != "all":
+            prompt_responses = prompt_responses.filter(language__code=selected_lang_code)
+
+        context.update(
+            {
+                "language_filters": language_filter_choices,
+                "selected_lang_code": selected_lang_code,
+                "prompt_responses": prompt_responses,
+            }
+        )
+
+        return context
 
 
 prompt_detail_view = PromptDetailView.as_view()
