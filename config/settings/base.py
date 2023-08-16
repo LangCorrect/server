eafec3173dd9b1edc9f77cc02271a4bde4608eea
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-08-16 00:31:12 -0700

    Fixed URL query for UI Avatars API (#76)

--- config/settings/base.py
+++ config/settings/base.py
@@ -355,6 +355,6 @@ ROSETTA_MESSAGES_PER_PAGE = 25
 ROSETTA_SHOW_AT_ADMIN_PANEL = True
 ROSETTA_SHOW_OCCURRENCES = False
 
-AVATAR_BASE_URL = "https://ui-avatars.com/api/rounded=true&length=1&name="
+AVATAR_BASE_URL = "https://ui-avatars.com/api/?rounded=true&length=1&name="
 
 DJANGO_NOTIFICATIONS_CONFIG = {"USE_JSONFIELD": True}
