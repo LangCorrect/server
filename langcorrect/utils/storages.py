5c3e777bc35d893692d70ac2a53ebe889a45fad0
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-08-19 02:59:38 -0700

    Add utility functions for S3 media handling

--- langcorrect/utils/storages.py
+++ langcorrect/utils/storages.py
@@ -1,3 +1,9 @@
+import uuid
+from io import BytesIO
+
+from django.core.files import File
+from django.core.files.uploadedfile import UploadedFile
+from PIL import Image
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
@@ -9,3 +15,37 @@ class StaticRootS3Boto3Storage(S3Boto3Storage):
 class MediaRootS3Boto3Storage(S3Boto3Storage):
     location = "media"
     file_overwrite = False
+
+
+class S3MediaStorageUtility:
+    @staticmethod
+    def generate_unique_filename(extension: str) -> str:
+        """Generate a unique filename using UUID."""
+        return f"{uuid.uuid4()}.{extension}"
+
+    @staticmethod
+    def get_file_extension(file_obj: UploadedFile) -> str:
+        return file_obj.name.split(".")[-1]
+
+    @staticmethod
+    def compress_image(file_obj: UploadedFile, quality=40) -> UploadedFile:
+        """Compress an image to the specified quality and return the compressed image."""
+        ext = S3MediaStorageUtility.get_file_extension(file_obj)
+        new_name = S3MediaStorageUtility.generate_unique_filename(ext)
+
+        image = Image.open(file_obj)
+        output = BytesIO()
+
+        image_format = "JPEG" if ext.lower() in ["jpg", "jpeg"] else ext.upper()
+        image.save(output, format=image_format, quality=quality, optimize=True)
+
+        output.seek(0)
+        new_image = File(output, name=new_name)
+        return new_image
+
+    @staticmethod
+    def save_to_s3_media_storage(filename: str, file_obj: UploadedFile) -> str:
+        """Saves a file to the S3 media storage and returns the stored file's key."""
+        media_storage = MediaRootS3Boto3Storage()
+        media_storage.save(filename, file_obj)
+        return filename
