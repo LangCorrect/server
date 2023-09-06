49436dfbce7e33846d3fb8a9f812b54b48503cbb
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-09-06 00:28:19 -0700

    Create model to keep track of subscriptions

--- langcorrect/subscriptions/models.py
+++ langcorrect/subscriptions/models.py
@@ -11,15 +11,18 @@ class Product(models.TextChoices):
 
 
 class PaymentStatus(models.TextChoices):
-    PENDING = "P", _("Pending")
-    COMPLETED = "C", _("Completed")
+    AWAITING_PAYMENT = "A", _("Awaiting Payment")
+    PAID = "P", _("Paid")
     FAILED = "F", _("Failed")
 
 
 class StripeCustomer(SoftDeletableModel, TimeStampedModel):
     user = models.OneToOneField(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     customer_id = models.CharField(max_length=255)
-    subscription_id = models.CharField(max_length=255)
+    has_active_subscription = models.BooleanField(default=False)
+    current_subscription_id = models.CharField(max_length=255, blank=True, null=True)
+    premium_until = models.DateTimeField(null=True, blank=True)
+    ended_at = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.user.username
@@ -27,12 +30,15 @@ class StripeCustomer(SoftDeletableModel, TimeStampedModel):
 
 class PaymentHistory(SoftDeletableModel, TimeStampedModel):
     stripe_customer = models.ForeignKey(StripeCustomer, on_delete=models.CASCADE)
+    subscription_id = models.CharField(max_length=255)
+    session_id = models.CharField(max_length=255)
     product = models.CharField(choices=Product.choices, max_length=7)
+    product_id = models.CharField(max_length=255)
     status = models.CharField(choices=PaymentStatus.choices, max_length=1)
-    amount_paid = models.DecimalField(
+    amount_total = models.DecimalField(
         max_digits=5, decimal_places=2, validators=[MinValueValidator(1)]
     )  # stores up to 999.99
-    coupon_used = models.CharField(max_length=20, blank=True, null=True)
+    has_used_coupon = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
-        return f"{self.product} ({self.status}) ({self.amount_paid})"
+        return f"{self.product} ({self.status}) ({self.amount_total})"
