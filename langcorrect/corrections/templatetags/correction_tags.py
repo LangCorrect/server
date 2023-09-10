ffbc4aade986477645bf4e930e88d2d4d0597e5e
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-09-10 00:48:09 -0700

    Add multiple correction display settings

--- langcorrect/corrections/templatetags/correction_tags.py
+++ langcorrect/corrections/templatetags/correction_tags.py
@@ -16,3 +16,17 @@ def render_user_correction_card(user, current_user, post, correction_data):
         "replies": replies,
         "overall_feedback": overall_feedback,
     }
+
+
+@register.inclusion_tag("corrections/partials/side_by_side_corrections.html")
+def render_corrections_side_by_side(post):
+    all_post_rows = post.postrow_set.all
+
+    return {"all_post_rows": all_post_rows}
+
+
+@register.inclusion_tag("corrections/partials/sentence_by_sentence_corrections.html")
+def render_corrections_by_sentence(post):
+    all_post_rows = post.postrow_set.all
+
+    return {"all_post_rows": all_post_rows}
