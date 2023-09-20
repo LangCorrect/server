fb1f0b7140ece24a2e531ab9f1dddb175054d07f
keyxcode <keyxcode@gmail.com>
2023-09-20 18:59:20 +0700

    Add user prompts view boilerplate

--- langcorrect/prompts/views.py
+++ langcorrect/prompts/views.py
@@ -112,3 +112,56 @@ class PromptCreateView(CreateView):
 
 
 prompt_create_view = PromptCreateView.as_view()
+
+
+class UserPromptsView(LoginRequiredMixin, ListView):
+    model = Prompt
+    slug_field = "slug"
+    slug_url_kwarg = "slug"
+    paginate_by = 25
+
+    def get_mode(self):
+        return self.request.GET.get("mode", "open")
+
+    def get_lang_code(self):
+        return self.request.GET.get("lang_code", None)
+
+    def get_queryset(self):
+        current_user = self.request.user
+
+        qs = super().get_queryset().filter(language__in=current_user.all_languages)
+
+        mode = self.get_mode()
+        lang_code = self.get_lang_code()
+
+        if mode == "completed":
+            qs = qs.filter(post__user=current_user)
+        else:
+            qs = qs.exclude(post__user=current_user)
+
+        if lang_code and lang_code != "all":
+            qs = qs.filter(language__code=lang_code)
+
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        current_user = self.request.user
+
+        mode = self.get_mode()
+        selected_lang_code = self.get_lang_code()
+
+        language_filter_choices = current_user.all_languages
+
+        context.update(
+            {
+                "mode": mode,
+                "language_filters": language_filter_choices,
+                "selected_lang_code": selected_lang_code,
+            }
+        )
+
+        return context
+
+
+user_prompts_view = UserPromptsView.as_view()
