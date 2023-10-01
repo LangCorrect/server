96cb3b8ab0c1ca6e9271a2821e0922a9ea84039e
keyxcode <keyxcode@gmail.com>
2023-10-01 08:53:00 +0700

    Remove faulty url path

--- langcorrect/prompts/urls.py
+++ langcorrect/prompts/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from langcorrect.prompts.views import prompt_create_view, prompt_detail_view, prompt_list_view, user_prompts_view
+from langcorrect.prompts.views import prompt_create_view, prompt_detail_view, prompt_list_view
 
 app_name = "prompts"
 urlpatterns = [
     path("", view=prompt_list_view, name="list"),
     path("~create/", view=prompt_create_view, name="create"),
     path("<str:slug>/", view=prompt_detail_view, name="detail"),
-    path("me", view=user_prompts_view, name="user_prompts"),
 ]
