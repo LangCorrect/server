a144c3f00eb184bcf550f53ee5c03aa48a01eb33
keyxcode <keyxcode@gmail.com>
2023-10-06 01:46:19 +0700

    Implement verify_emails custom command

--- langcorrect/users/management/commands/verify_emails.py
+++ langcorrect/users/management/commands/verify_emails.py
@@ -1,8 +1,19 @@
 from django.core.management.base import BaseCommand
 
+from langcorrect.users.models import User
+
 
 class Command(BaseCommand):
     help = "Verify all email addresses for user accounts that have the flag is_verified set to True"
 
     def handle(self, *args, **options):
-        print("hello")
+        verified_users = User.objects.filter(is_verified=True)
+
+        for user in verified_users:
+            verified_user_emails = user.emailaddress_set.all()
+            try:
+                for email in verified_user_emails:
+                    email.verified = True
+                    email.save()
+            except Exception as e:
+                print(e)
