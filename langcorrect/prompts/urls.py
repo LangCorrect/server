c555f27a36c4ab4a442cd68fb9abba81c6f7ab8a
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-12-04 14:53:04 +0100

    Add mod tool to convert prompt to journal entries (#141)

--- langcorrect/prompts/urls.py
+++ langcorrect/prompts/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
-from langcorrect.prompts.views import prompt_create_view, prompt_detail_view, prompt_list_view
+from langcorrect.prompts.views import (
+    convert_prompt_to_journal_entry,
+    prompt_create_view,
+    prompt_detail_view,
+    prompt_list_view,
+)
 
 app_name = "prompts"
 urlpatterns = [
     path("", view=prompt_list_view, name="list"),
     path("~create/", view=prompt_create_view, name="create"),
     path("<str:slug>/", view=prompt_detail_view, name="detail"),
+    path("convert_to_journal/<str:slug>/", convert_prompt_to_journal_entry, name="convert_to_journal"),
 ]
