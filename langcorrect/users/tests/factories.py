270575045a028131b98700ef89da9aac7d0ae718
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2024-01-01 05:59:20 +0100

    Update UserFactory to create LanguageLevels

--- langcorrect/users/tests/factories.py
+++ langcorrect/users/tests/factories.py
@@ -1,9 +1,12 @@
+import random
 from collections.abc import Sequence
 from typing import Any
 
 from django.contrib.auth import get_user_model
 from factory import Faker, post_generation
 from factory.django import DjangoModelFactory
+
+from langcorrect.languages.tests.utils import create_native_languages, create_studying_languages
 
 
 class UserFactory(DjangoModelFactory):
@@ -27,6 +30,17 @@ class UserFactory(DjangoModelFactory):
             ).evaluate(None, None, extra={"locale": None})
         )
         self.set_password(password)
+
+    @post_generation
+    def languages(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        for _ in range(random.randint(1, 2)):
+            create_studying_languages(self)
+
+        for _ in range(random.randint(1, 3)):
+            create_native_languages(self)
 
     class Meta:
         model = get_user_model()
