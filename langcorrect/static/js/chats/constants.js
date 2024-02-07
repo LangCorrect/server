09e9c5e8849044c841a481882e0896d90a0c5dfc
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2024-02-07 01:54:14 -0800

    Add message type constants

---
+++ langcorrect/static/js/chats/constants.js
@@ -0,0 +1,12 @@
+export const MessageTypes = {
+  WentOnline: 1,
+  WentOffline: 2,
+  TextMessage: 3,
+  FileMessage: 4,
+  IsTyping: 5,
+  MessageRead: 6,
+  ErrorOccurred: 7,
+  MessageIdCreated: 8,
+  NewUnreadCount: 9,
+  TypingStopped: 10,
+};
