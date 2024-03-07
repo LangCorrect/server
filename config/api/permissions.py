0f7e12db507996c78f8daf6ff5d807f662478f4f
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2024-03-06 22:12:59 -0800

    Add CanViewPost permission to check post visibility

--- config/api/permissions.py
+++ config/api/permissions.py
@@ -1,5 +1,7 @@
 from rest_framework import permissions
 
+from langcorrect.posts.models import PostVisibility
+
 
 class IsOwnerOrStaff(permissions.BasePermission):
     """
@@ -8,3 +10,17 @@ class IsOwnerOrStaff(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user or request.user.is_staff
+
+
+class CanViewPost(permissions.BasePermission):
+    message = "You do not have permission to view this post."
+
+    def has_object_permission(self, request, view, obj):
+        if (
+            obj.permission == PostVisibility.PUBLIC
+            or obj.permission == PostVisibility.MEMBER
+            and request.user.is_authenticated
+        ):
+            return True
+        else:
+            return False
