dceea6b40d1f4e5f090e54ce8b04f76cea8585b3
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2024-03-06 20:24:31 -0800

    Update router configuration to remove trailing slashes

--- config/api_router.py
+++ config/api_router.py
@@ -5,7 +5,11 @@ from rest_framework.routers import SimpleRouter
 from langcorrect.posts.api.views import PostViewSet
 from langcorrect.users.api.views import UserViewSet
 
-router = DefaultRouter() if settings.DEBUG else SimpleRouter()
+router = (
+    DefaultRouter(trailing_slash=False)
+    if settings.DEBUG
+    else SimpleRouter(trailing_slash=False)
+)
 
 router.register("users", UserViewSet)
 router.register("posts", PostViewSet)
