0f0e4a01e90c8db463da45ccf3ec8b710cc6a95d
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2024-03-15 15:39:41 -0700

    Update database configuration and static file storage

--- config/settings/production.py
+++ config/settings/production.py
@@ -73,7 +73,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
