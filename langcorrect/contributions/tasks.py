696f84fafb183208d248742225f30c9a77d19e22
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2024-05-11 17:32:11 -0700

    perf(calculate_rankings): Process calculations in batches of 500

--- langcorrect/contributions/tasks.py
+++ langcorrect/contributions/tasks.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.core.paginator import Paginator
 from django.db import transaction
 
 from config import celery_app
@@ -13,49 +14,50 @@ def calculate_rankings():
     """
     A Celery task to calculate user rankings based on total contributions.
     """
-    contributions = (
-        Contribution.available_objects.select_related("user")
-        .prefetch_related(
-            "user__post_set",
-            "user__prompt_set",
-        )
-        .all()
+    contributions = Contribution.available_objects.select_related(
+        "user",
+    ).prefetch_related(
+        "user__post_set",
+        "user__prompt_set",
     )
 
-    with transaction.atomic():
-        contribution_updates = []
+    paginator = Paginator(contributions, 500)
 
-        for contribution in contributions:
-            posts_count = contribution.user.post_set.count()
-            prompts_count = contribution.user.prompt_set.count()
-            corrections_count = contribution.user.corrections_made_count
-            total_points = posts_count + corrections_count + prompts_count
+    for page_number in paginator.page_range:
+        page = paginator.page(page_number)
+        with transaction.atomic():
+            contribution_updates = []
 
-            contribution_updates.append(
-                Contribution(
-                    id=contribution.id,
-                    total_points=total_points,
-                    post_count=posts_count,
-                    correction_count=corrections_count,
-                    prompt_count=prompts_count,
-                ),
+            for contribution in page.object_list:
+                posts_count = contribution.user.post_set.count()
+                prompts_count = contribution.user.prompt_set.count()
+                corrections_count = contribution.user.corrections_made_count
+                total_points = posts_count + corrections_count + prompts_count
+
+                contribution_updates.append(
+                    Contribution(
+                        id=contribution.id,
+                        total_points=total_points,
+                        post_count=posts_count,
+                        correction_count=corrections_count,
+                        prompt_count=prompts_count,
+                    ),
+                )
+
+            Contribution.objects.bulk_update(
+                contribution_updates,
+                ["total_points", "post_count", "correction_count", "prompt_count"],
             )
 
-        Contribution.objects.bulk_update(
-            contribution_updates,
-            ["total_points", "post_count", "correction_count", "prompt_count"],
+    ranking_updates = []
+    current_rank = 1
+    for contribution in Contribution.available_objects.order_by("-total_points"):
+        ranking_updates.append(
+            Contribution(id=contribution.id, rank=current_rank),
         )
+        current_rank += 1
 
-        ranking_updates = []
-        current_rank = 1
-
-        for contribution in Contribution.available_objects.order_by("-total_points"):
-            ranking_updates.append(
-                Contribution(id=contribution.id, rank=current_rank),
-            )
-            current_rank += 1
-
-        Contribution.objects.bulk_update(ranking_updates, ["rank"])
+    Contribution.objects.bulk_update(ranking_updates, ["rank"])
 
 
 @celery_app.task()
