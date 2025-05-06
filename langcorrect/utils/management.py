d9ec4517027712f04ef446fdeff604f7a8bb2690
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2025-05-06 21:46:02 +0900

    refactor(DataManagement): use static method when validating user inputs for migration

--- langcorrect/utils/management.py
+++ langcorrect/utils/management.py
@@ -10,16 +10,20 @@ from langcorrect.prompts.models import Prompt
 from langcorrect.users.exceptions import MissingSystemUserError
 
 NOT_SYSTEM_USER_ERROR = "Target user must be a system user"
+MIGRATION_MISSING_USERS_ERROR = "Missing from_user or to_user"
 
 
 class DataManagement:
     @staticmethod
-    def migrate_posts(from_user, to_user, require_system_user=True):
+    def _validate_users(from_user, to_user, require_system_user=True):
         if not from_user or not to_user:
-            return
-
+            raise ValueError(MIGRATION_MISSING_USERS_ERROR)
         if require_system_user and not to_user.is_system:
-            raise MissingSystemUserError()
+            raise MissingSystemUserError(NOT_SYSTEM_USER_ERROR)
+
+    @staticmethod
+    def migrate_posts(from_user, to_user, require_system_user=True):
+        DataManagement._validate_users(from_user, to_user, require_system_user)
 
         with transaction.atomic():
             Post.all_objects.filter(user=from_user).update(user=to_user)
@@ -35,33 +39,21 @@ class DataManagement:
 
     @staticmethod
     def migrate_prompts(from_user, to_user, require_system_user=True):
-        if not from_user or not to_user:
-            return
-
-        if require_system_user and not to_user.is_system:
-            raise MissingSystemUserError(NOT_SYSTEM_USER_ERROR)
+        DataManagement._validate_users(from_user, to_user, require_system_user)
 
         with transaction.atomic():
             Prompt.all_objects.filter(user=from_user).update(user=to_user)
 
     @staticmethod
     def migrate_corrections(from_user, to_user, require_system_user=True):
-        if not from_user or not to_user:
-            return
-
-        if require_system_user and not to_user.is_system:
-            raise MissingSystemUserError(NOT_SYSTEM_USER_ERROR)
+        DataManagement._validate_users(from_user, to_user, require_system_user)
 
         with transaction.atomic():
             PostUserCorrection.all_objects.filter(user=from_user).update(user=to_user)
 
     @staticmethod
     def migrate_comments(from_user, to_user, require_system_user=True):
-        if not from_user or not to_user:
-            return
-
-        if require_system_user and not to_user.is_system:
-            raise MissingSystemUserError(NOT_SYSTEM_USER_ERROR)
+        DataManagement._validate_users(from_user, to_user, require_system_user)
 
         with transaction.atomic():
             Comment.all_objects.filter(user=from_user).update(user=to_user)
